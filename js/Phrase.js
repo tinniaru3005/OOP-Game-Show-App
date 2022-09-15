3621c7bde9826037c66a11dcd376fa0e1dd18a41
Arunima Chaudhuri <arunimachaudhuri2020@gmail.com>
2022-09-16 00:28:46 +0530

    Add addPhraseToDisplay()

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,8 +1,23 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
 class Phrase {
     constructor(phrase){
         this.phrase = phrase.toLowerCase();
     }
+    addPhraseToDisplay() {
+        const phraseArr = document.getElementById('phrase');
+        const phrase_ul = document.querySelector('ul');
+        for(let i = 0; i < phraseArr.length; i++) {
+            if(phraseArr[i] === ' '){
+                const space_li = document.createElement('li');
+                space_li.className = 'space';
+                phrase_ul.append(space_li);
+            } else {   
+                const letter_li = document.createElement('li');
+                letter_li.className = 'hide letter';
+                letter_li.textContent = `${phraseArr[i]}`;
+                phrase_ul.append(letter_li);
+            }
+        }
+        return phrase_ul; 
+    }
 }
+
