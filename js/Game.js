de8555b58fb9df3666b2e997d78a6049f7c2ee2b
Arunima Chaudhuri <arunimachaudhuri2020@gmail.com>
2022-09-16 01:46:44 +0530

    Class & constructor

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,13 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+    constructor(){
+        this.missed = 0;
+        this.phrases = [
+            new Phrase('Set yourself free'),
+            new Phrase('Day dreamer'), 
+            new Phrase('Know your worth'), 
+            new Phrase('Make it happen'), 
+            new Phrase('Be kind')
+        ];
+        this.activePhrase = null; 
+    }
+}
