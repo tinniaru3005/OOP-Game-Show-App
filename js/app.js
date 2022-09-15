5a1851225c2ba77703b2eb1354175404e2925a86
Arunima Chaudhuri <arunimachaudhuri2020@gmail.com>
2022-09-16 03:04:02 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+let game = null;  
+const startButton = document.getElementById('btn__reset');
+const keys = document.getElementsByClassName('key');
+
+startButton.addEventListener('click', () => {
+    game = new Game()
+    game.startGame();
+});
+
+function buttonClicked() {
+    for(let i = 0; i < keys.length; i++) {
+        keys[i].addEventListener('click', (e) => {
+            const button = e.target;
+            game.handleInteraction(button);
+        })
+    };
+}
+buttonClicked();
