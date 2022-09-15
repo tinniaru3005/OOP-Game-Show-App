835598ce2d5f69dc52cc65aea3ecfabb61235be1
Arunima Chaudhuri <arunimachaudhuri2020@gmail.com>
2022-09-16 02:52:00 +0530

    Add handleInteraction(button)

--- js/Game.js
+++ js/Game.js
@@ -26,7 +26,7 @@ class Game {
     checkForWin(letter) {
         let phraseLetters = document.getElementsByClassName('letter');
         let shownLetters = document.getElementsByClassName('show');
-        if(shownLetters.length === phraseLetters.length) {
+        if (shownLetters.length === phraseLetters.length) {
             return true; 
         } else {
             return false; 
@@ -54,23 +54,38 @@ class Game {
         const screenOverlay = document.getElementById("overlay");
         let gameOverMessage = document.getElementById('game-over-message');
         screenOverlay.style.display = '';
-        if(gameWon) {
+        if (gameWon) {
             screenOverlay.className = 'win';
-           gameOverMessage.innerHTML = `Great job, you've correctly figured out the phrase!`;
+           gameOverMessage.innerHTML = `Congratulations! That's the correct phrase!`;
         } else {
             screenOverlay.className = 'lose';
-            gameOverMessage.innerHTML = `Bummer, you didn't correctly figure out the phrase. Give it another shot!`;
+            gameOverMessage.innerHTML = `Sorry, That's not the correct phrase. Try again!`;
         }
         this.resetGame();
     }
 
     removeLife() {
         const heartsLeft = document.querySelectorAll('img');
-        if(this.missed < 4) {
+        if (this.missed < 4) {
             heartsLeft[this.missed].src = 'images/lostHeart.png';
             this.missed += 1
         } else {
             this.gameOver();
+        }
+    }
+
+    handleInteraction(button) {
+        button.disabled = true;
+        if (!this.activePhrase.phrase.includes(button.textContent)) {
+            button.className = 'wrong';
+            this.removeLife();
+        } else {
+            button.className = 'chosen';
+            this.activePhrase.showMatchedLetter(button.textContent);
+            this.checkForWin()
+            if(this.checkForWin()) {
+                this.gameOver(true) 
+            }
         }
     }
 }
